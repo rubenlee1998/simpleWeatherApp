ace226c16c5373218893bd442b46aca05ad7fd93
RubenBoard <klsj9810@gmail.com>
2023-01-28 06:11:53 +0900

    ~2.6

--- App.js
+++ App.js
@@ -1,25 +1,81 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Dimensions, ScrollView, StyleSheet, Text, View } from "react-native";
+
+const tdingColor = "#AB6FE3";
+const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hello Ruben!</Text>
-      <Text>This is an app created by React Native! 🚀</Text>
-
-      <StatusBar style="auto" />
+      <StatusBar style="light" backgroundColor={tdingColor} />
+      <View style={styles.city}>
+        <Text style={styles.cityName}>seoul</Text>
+      </View>
+      <ScrollView
+        pagingEnabled
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.weather}
+      >
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+      </ScrollView>
     </View>
   );
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: tdingColor,
   },
-  text: {
-    fontSize: 28,
-    color: "red",
+  city: {
+    flex: 1,
+    backgroundColor: "#8A2BE2",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cityName: {
+    color: "white",
+    fontSize: 68,
+    fontWeight: "500",
+  },
+  weather: {
+    backgroundColor: "teal",
+  },
+  day: {
+    width: SCREEN_WIDTH,
+    alignItems: "center",
+    backgroundColor: "teal",
+  },
+  temp: {
+    color: "white",
+    backgroundColor: "#0055FF",
+    marginTop: 50,
+    fontSize: 178,
+  },
+  description: {
+    backgroundColor: "#6667AB",
+    color: "white",
+    marginTop: -30,
+    fontSize: 60,
   },
 });
